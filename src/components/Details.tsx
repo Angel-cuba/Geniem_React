c3265862fbea145fb1ac28ff58ced438fe582f01
Mac_Developer <araozangel842@gmail.com>
2022-03-28 21:55:38 +0300

    Using params to fetch data by id

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,20 +1,42 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { ReaderById } from '../api/request';
+import { IList } from '../interfaces/interfaces';
+import { useStyles } from '../styles/styles';
 
 const Details = () => {
 	const { id } = useParams();
+	const styles = useStyles();
+	const [itemId, setItemId] = useState<IList>();
+	console.log(itemId);
 
 	useEffect(() => {
 		ReaderById(id)
 			.then((response) => response.json())
 			.then((data) => {
-				console.log(data.title);
+				setItemId(data);
 			});
 	}, [id]);
 
-	console.log(id);
-	return <div>Details</div>;
+	return (
+		<div className={styles.details}>
+			<h1>Details</h1>
+			{!itemId ? (
+				'Loading...'
+			) : (
+				<div className={styles.contentDetails}>
+					<p className={styles.paragraphDetails}>
+						This picture is available on the album{' '}
+						<span className={styles.spanDetails}>{itemId.albumId}</span>
+					</p>
+					<img src={itemId.url} alt={`Pic with id ${itemId.id}`} />
+				</div>
+			)}
+			<Link className={styles.detailsLink} to="/">
+				Back to all pictures
+			</Link>
+		</div>
+	);
 };
 
 export default Details;
